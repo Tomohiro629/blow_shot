913ccee9a2e2c7716814f2856a2a127958bcbdf0
Tomohiro629 <radcell0101@gmail.com>
2022-10-15 11:15:30 +0900

    screenUtilInit追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:blow_shot/app/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(ProviderScope(
+      child: ScreenUtilInit(
+          designSize: const Size(360, 690),
+          builder: (BuildContext context, child) => const MyApp())));
 }
 
 class MyApp extends StatelessWidget {
