d66f82f014b9a0428135310afe1f2c3a59efc64d
Tomohiro629 <radcell0101@gmail.com>
2022-10-17 13:26:16 +0900

    inputformレイアウト変更

--- lib/app/components/input_form.dart
+++ lib/app/components/input_form.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
 
 class InputForm extends StatelessWidget {
-  const InputForm({super.key, required this.onTap, required this.text});
+  const InputForm({
+    super.key,
+    required this.onTap,
+    required this.text,
+  });
   final void Function() onTap;
   final String text;
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -13,20 +20,42 @@ class InputForm extends StatelessWidget {
               const EdgeInsets.symmetric(vertical: 100.0, horizontal: 20.0),
           child: Container(
             decoration: const BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                    color: Color.fromARGB(220, 255, 255, 255),
+                    spreadRadius: 5,
+                    offset: Offset(1, 1))
+              ],
               borderRadius: BorderRadius.all(Radius.circular(20)),
-              color: Colors.white,
             ),
           ),
         ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(),
-            ElevatedButton(
-              onPressed: onTap,
-              child: Text(text),
-            )
-          ],
+        Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 250.w,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "メールアドレス"),
+                ),
+              ),
+              const Gap(30),
+              SizedBox(
+                width: 250.w,
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                      border: OutlineInputBorder(), hintText: "パスワード"),
+                ),
+              ),
+              const Gap(50),
+              ElevatedButton(
+                onPressed: onTap,
+                child: Text(text),
+              )
+            ],
+          ),
         ),
       ],
     );
