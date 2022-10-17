acd5e93c1fff32a5c2a158d30a3137ab4602affe
Tomohiro629 <radcell0101@gmail.com>
2022-10-17 09:55:41 +0900

    loquid_swipe追加

--- lib/app/home_page/home_page.dart
+++ lib/app/home_page/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:blow_shot/app/components/page_back_ground.dart';
-import 'package:blow_shot/app/sign_in_page/sign_in_page.dart';
-import 'package:blow_shot/app/sign_up_page/sign_up_page.dart';
+import 'package:blow_shot/app/liquid_swipe_page/liquid_swipe_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -60,18 +59,10 @@ class HomePage extends StatelessWidget {
                     onPressed: () {
                       Navigator.of(context)
                           .push(MaterialPageRoute(builder: (context) {
-                        return const SignInPage();
+                        return const LiquidSwipePage();
                       }));
                     },
-                    child: const Text("ログイン")),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context)
-                          .push(MaterialPageRoute(builder: (context) {
-                        return const SignUpPage();
-                      }));
-                    },
-                    child: const Text("サインアップ")),
+                    child: const Text("始める")),
               ],
             ),
           ],
