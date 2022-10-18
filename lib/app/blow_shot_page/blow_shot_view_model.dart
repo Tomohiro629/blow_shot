40fd2f908da70929faa0a841f59e3bf700a2fd1e
Tomohiro629 <radcell0101@gmail.com>
2022-10-18 19:43:39 +0900

    blow_shot_view_model作成

---
+++ lib/app/blow_shot_page/blow_shot_view_model.dart
@@ -0,0 +1,22 @@
+import 'package:blow_shot/business/model/photo_model.dart';
+import 'package:blow_shot/business/repository/photo_repository.dart';
+import 'package:blow_shot/service/auth_service.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final blowShotViewModel = ChangeNotifierProvider<BlowShotViewModel>((ref) {
+  return BlowShotViewModel(
+    ref.read,
+  );
+});
+
+class BlowShotViewModel extends ChangeNotifier {
+  final Reader reader;
+  BlowShotViewModel(this.reader);
+
+  Future<void> setPhoto(imageURL) async {
+    final photo = Photo.createPhoto(
+        userId: reader(authServiceProvider).userId, imageURL: imageURL);
+    await reader(photoRepositoryProvider).setPhoto(photo);
+  }
+}
