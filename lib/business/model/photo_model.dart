406aa5e970f21bffba37667b7b3d69382dd0a9da
Tomohiro629 <radcell0101@gmail.com>
2022-10-18 09:26:01 +0900

    モデル名をPhotoに変えた

--- lib/business/model/photo_model.dart
+++ lib/business/model/photo_model.dart
@@ -1,22 +1,22 @@
 import 'package:uuid/uuid.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class Image {
+class Photo {
   final String id;
   final String imageURL;
   final DateTime timeStamp;
 
-  factory Image.createImage({
+  factory Photo.createPhoto({
     required String imageURL,
   }) {
-    return Image(
+    return Photo(
       id: const Uuid().v4(),
       imageURL: imageURL,
       timeStamp: DateTime.now(),
     );
   }
-  factory Image.fromJson(Map<String, dynamic> map) {
-    return Image(
+  factory Photo.fromJson(Map<String, dynamic> map) {
+    return Photo(
       id: map['id'],
       imageURL: map['imageURL'],
       timeStamp: (map['timeStamp'] as Timestamp).toDate(),
@@ -30,5 +30,5 @@ class Image {
     };
   }
 
-  Image({required this.id, required this.imageURL, required this.timeStamp});
+  Photo({required this.id, required this.imageURL, required this.timeStamp});
 }
