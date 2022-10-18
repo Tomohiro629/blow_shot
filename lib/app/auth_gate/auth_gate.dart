66b0e74074da78a385acb752ea9375a95520a775
Tomohiro629 <radcell0101@gmail.com>
2022-10-18 20:21:55 +0900

    auth_gate追加

---
+++ lib/app/auth_gate/auth_gate.dart
@@ -0,0 +1,23 @@
+import 'package:blow_shot/app/blow_shot_page/blow_shot_page.dart';
+import 'package:blow_shot/app/sign_in_page/sign_in_page.dart';
+import 'package:blow_shot/service/auth_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class AuthGate extends ConsumerWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final authUser = ref.watch(authServiceProvider).getAuthState;
+
+    return StreamBuilder<User?>(
+      stream: authUser,
+      builder: ((context, snapshot) {
+        final isLoggedIn = snapshot.data != null;
+        return isLoggedIn ? const BlowShotPage() : const SignInPage();
+      }),
+    );
+  }
+}
