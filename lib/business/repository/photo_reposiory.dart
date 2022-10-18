d7495285a1bf154f3a94af33aca691ee682a21be
Tomohiro629 <radcell0101@gmail.com>
2022-10-18 09:32:39 +0900

    photoRepository作成

---
+++ lib/business/repository/photo_reposiory.dart
@@ -0,0 +1,25 @@
+import '../model/photo_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final photoRepositoryProvider = Provider<PhotoRepository>((ref) {
+  return PhotoRepository();
+});
+
+class PhotoRepository {
+  final _firestore = FirebaseFirestore.instance;
+
+  Stream<List<Photo>> fetchPhotoStream() {
+    final snapshots = _firestore.collection('photos').snapshots();
+    return snapshots.map(((qs) => qs.docs.isEmpty
+        ? []
+        : qs.docs.map((doc) => Photo.fromJson(doc.data())).toList()));
+  }
+
+  Future<void> setPhoto(Photo photo) async {
+    await _firestore
+        .collection('photos')
+        .doc(photo.id)
+        .set(photo.toJson(), SetOptions(merge: true));
+  }
+}
