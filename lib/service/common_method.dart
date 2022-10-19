9ae4d457c85c19c36152a1debceb1f569087a26b
Beppu Tomohiro <98255334+Tomohiro629@users.noreply.github.com>
2022-10-19 12:44:51 +0900

    不要な関数削除

--- lib/service/common_method.dart
+++ lib/service/common_method.dart
@@ -3,11 +3,3 @@ import 'package:intl/intl.dart';
 String getDateString(DateTime date) {
   return DateFormat("yyyy年MM月dd日").format(date);
 }
-
-String getRecordTime(DateTime date) {
-  return DateFormat("HH時mm分").format(date);
-}
-
-String upDateDateString(DateTime date) {
-  return DateFormat("yyyyMMdd").format(date);
-}
