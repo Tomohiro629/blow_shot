7fd196af4fbb8db4a00115f2ed9e488a2ad55f1b
Tomohiro629 <radcell0101@gmail.com>
2022-10-26 10:41:27 +0900

    swiped_viewデモ作成

---
+++ lib/sairi_swiped.dart
@@ -0,0 +1,44 @@
+import 'package:blow_shot/app/components/app_colors.dart';
+import 'package:blow_shot/app/components/base_app_bar.dart';
+import 'package:blow_shot/app/components/page_back_ground.dart';
+import 'package:blow_shot/sairi_photos.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_swiper_view/flutter_swiper_view.dart';
+
+class SairiSwiped extends ConsumerWidget {
+  const SairiSwiped({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: const BaseAppBar(
+        title: Text("さいりコレクション"),
+        widgets: [],
+        color: Color.fromARGB(165, 220, 94, 136),
+      ),
+      body: PageBackGround(
+        colors: const [Colors.pink, AppColors.secondary],
+        page: Swiper(
+          layout: SwiperLayout.TINDER,
+          itemBuilder: ((context, index) {
+            return ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.network(
+                  images[index % images.length],
+                  fit: BoxFit.cover,
+                ));
+          }),
+          index: 0,
+          itemCount: images.length,
+          itemWidth: 300.0.w,
+          itemHeight: 500.0.h,
+          pagination: const SwiperPagination(),
+          // control: const SwiperControl(),
+          autoplay: false,
+          loop: true,
+        ),
+      ),
+    );
+  }
+}
